7d69ca4337bcb94214d86052798b4108df6cf9e0
Wolfrax <Rafmagn1>
2020-07-24 14:38:15 +0200

    Updates

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Installation
+A collection of python scripts to recurrently collect and store meteorological observations by 
+[SMHI](https://www.smhi.se/data) and to visualize some observations.
 
-Install requests and geojson, see requirements.txt
-```shell script
-# On Linux Mint
-pip freeze > requirements.txt
-# On Raspberry (rpi5.local)
-python3 -m venv .venv
-source .venv/bin/activate
-# Now install
-pip install -r requirements.txt
-```
+![](Screenshot.png)
+
+The observations are stored in GeoJSON format are available through [web page](https://www.viltstigen.se/smhi_metobs/) 
+or using URL's. 
+See detailed description [here](https://wlog.viltstigen.se/articles/2020/07/05/smhi-meteorological-observations/).
+Script, see `py/collector_metobs.py` and `py/emitter_metobs.py`.
+
+For visualizations, see [https://www.viltstigen.se/smhi_metobs/weather.html]
+Script, see `py/swe_weather.py`
+
+For installation of geopandas and geoplot, see [https://wlog.viltstigen.se/articles/2020/07/07/installing-geopandas-and-geoplot-on-raspberry-pi/]
--- py/swe_weather.py
+++ py/swe_weather.py
@@ -8,6 +8,7 @@ __author__ = 'mm'
 #
 
 import matplotlib.pyplot as plt
+import matplotlib.streamplot as splt
 import geopandas as gpd
 import numpy as np
 import os
@@ -27,116 +28,157 @@ METOBS_DIR = "metobs_data"
 app = Flask(__name__)
 
 
-def add_windstreams(fig, ax, plot_type):
-    wind_directions = gpd.read_file("https://www.viltstigen.se/metobs/latest/03*")  # Probl in EPSG 3006 or 3021
-    wind_speeds = gpd.read_file("https://www.viltstigen.se/metobs/latest/04*")
-    wind_stations = gpd.overlay(wind_directions, wind_speeds, how='intersection')
+class Map:
+    def __init__(self):
+        self.world = gpd.read_file(os.path.join(DATA_DIR, "ne_50m_admin_0_countries.shp"))
+        self.country = None
+        self.min_x = None
+        self.min_y = None
+        self.max_x = None
+        self.max_y = None
 
-    x = wind_stations.geometry.x.to_numpy()
-    y = wind_stations.geometry.y.to_numpy()
-
-    # Note, wind directions: Wind from West to East = 270 dgr
-    # We need to map vectors to cartesian x - and y-axis using cos and sin
-    directions = np.deg2rad(wind_stations['value_1'] - 270)
-    u = (wind_stations['value_2'] * np.cos(directions)).to_numpy()
-    v = (wind_stations['value_2'] * np.sin(directions)).to_numpy()
-
-    magnitude = (u ** 2 + v ** 2) ** 0.5
-    if plot_type == 'Stream':
-        img = ax.streamplot(x, y, u, v, transform=ccrs.PlateCarree(), color=magnitude)
-    else:
-        img = ax.quiver(x, y, u, v, magnitude, transform=ccrs.PlateCarree())
-
-    fig.colorbar(img.lines if plot_type == 'Stream' else img)
-
-
-def imshow(fig, ax, data, bounds, col_map, add_contour=True, add_colorbar=True):
-    x = data.geometry.x.to_numpy()  # X coordinate/latitude for station
-    y = data.geometry.y.to_numpy()
-
-    grid_x = np.arange(bounds[0]-DELTA, bounds[2]+DELTA, 0.1)
-    grid_y = np.arange(bounds[1]-DELTA, bounds[3]+DELTA, 0.1)
-    xi, yi = np.meshgrid(grid_x, grid_y)
-
-    z = data['value'].to_numpy()
-    zi = griddata((x, y), z, (xi, yi), method='linear')
-
-    im = ax.imshow(zi, cmap=col_map, extent=[bounds[0]-DELTA, bounds[2]+DELTA, bounds[1]-DELTA, bounds[3]+DELTA],
-                   interpolation='None', origin='lower', transform=ccrs.PlateCarree(), zorder=1)
-    if add_contour:
-        cnt = ax.contour(xi, yi, zi, 5, linewidths=0.25, colors='black', transform=ccrs.PlateCarree(), zorder=2)
-        ax.clabel(cnt, colors='black', inline=True, fontsize=10, fmt="%i")
-    if add_colorbar:
-        fig.colorbar(im)
-
-
-if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
-
-    world = gpd.read_file(os.path.join(DATA_DIR, "ne_50m_admin_0_countries.shp"))
-    swe = world[world['ADM0_A3'] == 'SWE']  # Filter out Sweden from the world
-
-    try:
         # We want the actual date as header, but data is generated at midnight the day after (read from meta data file)
         # Hence, we need to convert the date to yesterday's date to get it right.
         r = requests.get("https://www.viltstigen.se/smhi_metobs/latest/meta.json").json()
         today = r['generated'].split(" ")[0]
         yesterday = datetime.datetime.strftime(datetime.datetime.strptime(today, "%Y-%m-%d") -
                                                datetime.timedelta(days=1), "%Y-%m-%d")
-    except requests.exceptions.RequestException as e:
-        print("Error reading meta file")
-        yesterday = ""
+        self.title = yesterday
+
+        self.fig = plt.figure(figsize=(8, 6))
+        self.ax = plt.axes(projection=ccrs.AlbersEqualArea(central_latitude=62.3858, central_longitude=16.3220))
+
+        self.zorder = 0
+
+    def new_geometry(self, tag):
+        country = self.world[self.world['ADM0_A3'] == tag]
+        self.min_x = country.total_bounds[0] - DELTA
+        self.min_y = country.total_bounds[1] - DELTA
+        self.max_x = country.total_bounds[2] + DELTA
+        self.max_y = country.total_bounds[3] + DELTA
+        self.ax.set_extent([self.min_x, self.max_x, self.min_y, self.max_y])
+        return country
+
+    def add_geometry(self, g):
+        self.ax.add_geometries(g.geometry,
+                               edgecolor='black',
+                               facecolor='none',
+                               crs=ccrs.PlateCarree(),
+                               zorder=self.zorder)
+        self.zorder += 1
+
+    def gen_grid(self, data):
+        grid_x = np.arange(self.min_x, self.max_x, 0.1)
+        grid_y = np.arange(self.min_y, self.max_y, 0.1)
+
+        x, y = np.meshgrid(grid_x, grid_y)
+        z = griddata((data.geometry.x.to_numpy(), data.geometry.y.to_numpy()),
+                     data['value'].to_numpy(),
+                     (x, y),
+                     method='linear')
+        return {'x': x, 'y': y, 'z': z}
+
+    def add_image(self, gr, col_map):
+        pl = self.ax.imshow(gr['z'],
+                            cmap=col_map,
+                            extent=[self.min_x, self.max_x, self.min_y, self.max_y],
+                            interpolation='None',
+                            origin='lower',
+                            transform=ccrs.PlateCarree(),
+                            zorder=self.zorder)
+        self.zorder += 1
+        return pl
+
+    def add_contour(self, gr):
+        cnt = self.ax.contour(gr['x'], gr['y'], gr['z'], 5,
+                              linewidths=0.25, colors='black', transform=ccrs.PlateCarree(), zorder=self.zorder)
+        self.ax.clabel(cnt, colors='black', inline=True, fontsize=10, fmt="%i")
+        self.zorder += 1
+
+    def add_colorbar(self, image):
+        self.fig.colorbar(image.lines if isinstance(image, splt.StreamplotSet) else image)
+
+    def add_title(self, title_str):
+        self.ax.set_title(title_str)
+
+    def add_vectorfield(self, df, streampl):
+        x = df.geometry.x.to_numpy()
+        y = df.geometry.y.to_numpy()
+
+        # Note, wind directions: Wind from West to East = 270 dgr
+        # We need to map vectors to cartesian x - and y-axis using cos and sin
+        directions = np.deg2rad(df['value_1'] - 270)
+        u = (df['value_2'] * np.cos(directions)).to_numpy()
+        v = (df['value_2'] * np.sin(directions)).to_numpy()
+
+        magnitude = (u ** 2 + v ** 2) ** 0.5
+        if streampl:
+            pl = self.ax.streamplot(x, y, u, v, transform=ccrs.PlateCarree(), color=magnitude, zorder=self.zorder)
+        else:
+            pl = self.ax.quiver(x, y, u, v, magnitude, transform=ccrs.PlateCarree(), zorder=self.zorder)
+        self.zorder += 1
+        return pl
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
     images = []
+    obs_data = cmap = fname = fn = title = wind_stations = None
 
-    for obs in ['Temp', 'Rain', 'Pressure', 'Wind', 'Quiver']:
-        fig = plt.figure(figsize=(8, 6))
-        ax = plt.axes(projection=ccrs.AlbersEqualArea(central_latitude=62.3858, central_longitude=16.3220))
-        ax.set_extent([swe.total_bounds[0] - DELTA, swe.total_bounds[2] + DELTA,
-                       swe.total_bounds[1] - DELTA, swe.total_bounds[3] + DELTA])
-        ax.add_geometries(swe.geometry, edgecolor='black', facecolor='none', crs=ccrs.PlateCarree(), zorder=3)
+    for img in ['Temp', 'Rain', 'Pressure', 'Wind', 'Quiver']:
+        mp = Map()
 
-        if obs == 'Temp':
+        if img == 'Temp':
             obs_data = gpd.read_file("https://www.viltstigen.se/metobs/latest/02*")
             cmap = 'coolwarm'
             title = 'Average temperature 1 day'
             fname = 'temp.svg'
             fn = os.path.join(METOBS_DIR, IMG_DIR, fname)
-        elif obs == 'Rain':
+        elif img == 'Rain':
             obs_data = gpd.read_file("https://www.viltstigen.se/metobs/latest/05*")
             cmap = 'Blues'
             title = 'Rainfall 1 day'
             fname = 'rain.svg'
             fn = os.path.join(METOBS_DIR, IMG_DIR, fname)
-        elif obs == 'Pressure':
+        elif img == 'Pressure':
             obs_data = gpd.read_file("https://www.viltstigen.se/metobs/latest/09*")
             cmap = 'coolwarm'
             title = 'Air pressure, momentary value last hour'
             fname = 'pressure.svg'
             fn = os.path.join(METOBS_DIR, IMG_DIR, fname)
-        elif obs == 'Wind' or 'Quiver':
+        elif img == 'Wind' or 'Quiver':
             obs_data = gpd.read_file("https://www.viltstigen.se/metobs/latest/09*")
+            wind_directions = gpd.read_file("https://www.viltstigen.se/metobs/latest/03*")
+            wind_speeds = gpd.read_file("https://www.viltstigen.se/metobs/latest/04*")
+            wind_stations = gpd.overlay(wind_directions, wind_speeds, how='intersection')
             cmap = 'coolwarm'
-            title = 'Wind streams and air pressure' if obs == 'Wind' else "Wind direction and strengths"
-            fname = 'winds.svg' if obs == 'Wind' else "wind_quiver.svg"
+            title = 'Wind streams and air pressure' if img == 'Wind' else "Wind direction and strengths"
+            fname = 'winds.svg' if img == 'Wind' else "wind_quiver.svg"
             fn = os.path.join(METOBS_DIR, IMG_DIR, fname)
-        else:
-            obs_data = cmap = fn = fname = title = None
 
-        if obs == 'Wind':
-            imshow(fig, ax, obs_data, swe.total_bounds, cmap, add_contour=False, add_colorbar=False)
-            add_windstreams(fig, ax, plot_type='Stream')
-        elif obs == 'Quiver':
-            add_windstreams(fig, ax, plot_type='Quiver')
-        else:
-            imshow(fig, ax, obs_data, swe.total_bounds, cmap)
+        geom = mp.new_geometry('SWE')
+
+        if img in ['Temp', 'Rain', 'Pressure']:
+            grid = mp.gen_grid(obs_data)
+            im = mp.add_image(grid, cmap)
+            mp.add_contour(grid)
+            mp.add_colorbar(im)
+        elif img in ['Wind', 'Quiver']:
+            if img == 'Wind':
+                grid = mp.gen_grid(obs_data)
+                mp.add_image(grid, cmap)
+            mappable = mp.add_vectorfield(wind_stations, streampl=(img == 'Wind'))
+            mp.add_colorbar(mappable)
+
+        mp.add_title(title)
+        mp.add_geometry(geom)
 
         images.append(os.path.join(IMG_DIR, fname))
-        ax.set_title(title)
         plt.savefig(fn, bbox_inches='tight', pad_inches=0.1)
 
     html_file_name = os.path.join(METOBS_DIR, "weather.html")
     with app.app_context():
-        html_file = render_template('swe_weather.html', head=yesterday, images=images)
+        html_file = render_template('swe_weather.html', head=title, images=images)
         with open(html_file_name, encoding='utf-8', mode='w') as outfile:
             outfile.write(html_file)
